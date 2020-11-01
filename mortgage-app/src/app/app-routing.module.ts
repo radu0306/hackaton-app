57e6132dcb2ebd02e81b521701ac02c99fc82adc
Andreea <calinmadalina30@gmail.com>
2020-11-01 17:17:15 +0200

    Created welcome page -fe

--- mortgage-app/src/app/app-routing.module.ts
+++ mortgage-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GetStartedPageComponent } from './components/get-started-page/get-started-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'welcome-page', component: GetStartedPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mortgage-app/src/app/app.module.ts
+++ mortgage-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetStartedPageComponent } from './components/get-started-page/get-started-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetStartedPageComponent
   ],
   imports: [
     BrowserModule,
