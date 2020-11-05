047295d7b142a8558b6e2242d13b8287a02c4e5e
Andreea Calin <calinmadalina30@gmail.com>
2020-11-05 18:00:05 +0200

    Created login comp
    Created login comp

--- mortgage-app/src/app/app.module.ts
+++ mortgage-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GetStartedPageComponent } from './components/get-started-page/get-started-page.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GetStartedPageComponent
+    GetStartedPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
